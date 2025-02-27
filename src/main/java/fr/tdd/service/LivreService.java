9e403a28a690b9d5f12689e15fc5a6a7e686e14b
Brieuc C. <brieuc.courapie@free.fr>
2025-02-27 15:00:31 +0100

    Ajout CRUD livre

--- src/main/java/fr/tdd/service/LivreService.java
+++ src/main/java/fr/tdd/service/LivreService.java
@@ -2,9 +2,41 @@ package fr.tdd.service;
 
 import org.springframework.stereotype.Service;
 
+import fr.tdd.exception.LivreNotFoundException;
+import fr.tdd.model.Livre;
+import fr.tdd.repository.LivreRepository;
+
 @Service
 public class LivreService {
 
 
+    private final LivreRepository livreRepository;
+
+    public LivreService(LivreRepository livreRepository) {
+        this.livreRepository = livreRepository;
+    }
+
+    public Livre creerLivre(Livre livre) {
+        return livreRepository.save(livre);
+    }
+
+    public Livre obtenirLivre(String isbn) {
+        return livreRepository.findById(isbn)
+                .orElseThrow(() -> new LivreNotFoundException());
+    }
+
+    public Livre mettreAJourLivre(String isbn, Livre livreDetails) {
+        Livre livre = obtenirLivre(isbn);
+        livre.setTitre(livreDetails.getTitre());
+        livre.setAuteur(livreDetails.getAuteur());
+        livre.setEditeur(livreDetails.getEditeur());
+        livre.setDisponible(livreDetails.isDisponible());
+        return livreRepository.save(livre);
+    }
+
+    public void supprimerLivre(String isbn) {
+        Livre livre = obtenirLivre(isbn);
+        livreRepository.delete(livre);
+    }
 
 }
